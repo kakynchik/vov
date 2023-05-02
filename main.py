59ece47918e9e9d878fa1ea5d0981ed3445a3c07
kakynchik <ukakyn@gmail.com>
2023-05-02 10:39:15 -0700

    trete zavdannya

--- main.py
+++ main.py
@@ -7,8 +7,19 @@ def decorate(func):
 
 @decorate
 def my_func(x):
-    return x
 
-result = my_func(int(input("введіт число ")))
-print(result)
+def cache(func):
+    cache_dict = {}
+    def wrapper(*args, **kwargs):
+        if args in cache_dict:
+            return cache_dict[args]
+        else:
+            result = func(*args)
+            cache_dict[args] = result
+            return result, cache_dict
+        return wrapper
 
+@cache
+def my_func(x, y):
+    return x * y
+print(my_func(3, 10))

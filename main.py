66def0f1103cbfbf089ccf6d9586664cb5d33224
kakynchik <ukakyn@gmail.com>
2023-05-02 08:41:59 -0700

    pershiy priklad obgornytoi func

---
+++ main.py
@@ -0,0 +1,14 @@
+#теорія декоратирів
+
+def my_decorator_func(args): #3
+    def wrapper():
+        print("шото там")
+        func()
+        print("i tam shos")
+    return wrapper
+
+
+@my_decorator_func#2
+def say_hello():#1
+    print("hello!")
+say_hello()

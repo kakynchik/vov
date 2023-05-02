13b12e5e5da00943f0f83e024ec5b5e17b4223ef
kakynchik <ukakyn@gmail.com>
2023-05-02 09:02:24 -0700

    keshyvannya(fibanachi)

--- main.py
+++ main.py
@@ -13,4 +13,13 @@ def say_hello():#1
     print("hello!")
 say_hello()
 
-#
+
+def delay_decorator(func):
+    def wrapper(*args, **kwargs):
+        time.sleep(3)
+        return func(*args, **kwargs)
+    return wrapper
+@delay_decorator
+def sleepy():
+    print("ya splyu")
+sleepy()

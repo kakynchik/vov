ff461a683b0cf897fba830244370e797121d5482
kakynchik <ukakyn@gmail.com>
2023-05-02 10:20:17 -0700

    pershe zavdannya

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 #1
+def decorate(func):
+    def wragger(*args, **wragger):
+        result + func(*args, **wragger)
+        return result + 10
+    return wragger
 
-
-
-
-#2
-import time
-
-def time_function(func):
-    def wrapper(*args, **kwargs):
-        stat_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        print("Час виконання функцції ", end_time - stat_time)
-        return result
-    return wrapper
-
-
-@time_function
-def example_function(x):
-    time.sleep(1) #pass
+@decorate
+def my_func(x):
     return x
-print(example_function())
+
+result = my_func(int(input("введіт число ")))
+print(result)
+
